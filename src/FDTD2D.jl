d3dba631fed3e9f0821d1ed66e50c93d329c984d
Artem Korzhimanov <korzhimanov.artem@gmail.com>
2017-02-13 20:07:32 +0300

    Implemented calculate_params(init_params::Dict)

--- src/FDTD2D.jl
+++ src/FDTD2D.jl
@@ -1,15 +1,35 @@
 module FDTD2D
 export run
 
+"""Calculate parameters needed for simulation and return them as a dictionary."""
 function calculate_params(init_params::Dict)
-  return init_params
+  params = deepcopy(init_params)
+
+  params["box_size"] = Dict{String,Float64}()
+  params["box_size"]["x"] = params["x_bounds"][2] - params["x_bounds"][1]
+  params["box_size"]["y"] = params["y_bounds"][2] - params["y_bounds"][1]
+
+  params["space_step"] = Dict{String,Float64}()
+  params["space_step"]["x"] = params["box_size"]["x"]/params["matrix_size"]["x"]
+  params["space_step"]["y"] = params["box_size"]["y"]/params["matrix_size"]["y"]
+
+  params["time_step"] = 0.5*sqrt(params["space_step"]["x"]^2 + params["space_step"]["y"]^2)
+
+  params["half_cfl"] = Dict{String,Float64}()
+  params["half_cfl"]["x"] = 0.5*params["time_step"]/params["space_step"]["x"]
+  params["half_cfl"]["y"] = 0.5*params["time_step"]/params["space_step"]["y"]
+
+  params["y1"] = [0.0:params["matrix_size"]["y"]-1.0] * params["space_step"]["y"] + params["y_bounds"][1]
+  params["y2"] = [0.5:params["matrix_size"]["y"]-1.5] * params["space_step"]["y"] + params["y_bounds"][1]
+
+  return params
 end
 
 function init_data(params::Dict)
   return Dict{String, Number}()
 end
 
-"""Generate fields at left boundary (x_min = p['x_bounds'][0]) at time moment 'time'"""
+"""Generate fields at left boundary (x_min = p["x_bounds"][0]) at time moment 'time'"""
 function generate_fields_x_min!(d::Dict, time::Float64, p::Dict)
   return true
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,35 @@
 import FDTD2D
 using Base.Test
 
-@time @test FDTD2D.run(Dict("time_steps" => 2, "time_step" => 0.1)) == true
+function laser_pulse_gauss(x_min::Float64, width::Float64, duration::Float64)
+  function f(t::Float64, x::Float64, y::Float64)
+    return exp(-(y/width)^2)*exp(-((t - (x - x_min))/duration-2.0)^2)*sin(2.*pi*(t - x))
+  end
+  return f
+end
+
+params_correct = Dict(
+  "x_bounds" => (0.0, 8.0),
+  "y_bounds" => (-4.0, 4.0),
+  "matrix_size" => Dict("x" => 64, "y" => 64),
+  "time_steps" => 81,
+
+  "output" => Dict(
+    "iteration_pass" => 10,
+    "directory_name" => "test/output"
+  )
+)
+params_correct["laser_pulse_y_shape"] = laser_pulse_gauss(params_correct["x_bounds"][1], 1., 2.)
+params_correct["laser_pulse_z_shape"] = laser_pulse_gauss(params_correct["x_bounds"][1], 1., 2.)
+
+params_calculated = FDTD2D.calculate_params(params_correct)
+
+@test params_calculated["box_size"]["x"] == 8.0
+@test params_calculated["box_size"]["y"] == 8.0
+@test params_calculated["space_step"]["x"] == 0.125
+@test params_calculated["space_step"]["y"] == 0.125
+@test params_calculated["time_step"] == sqrt(2.0)/16.0
+@test params_calculated["half_cfl"]["x"] == 0.5*sqrt(0.5)
+@test params_calculated["half_cfl"]["y"] == 0.5*sqrt(0.5)
+
+@time @test FDTD2D.run(params_correct) == true
