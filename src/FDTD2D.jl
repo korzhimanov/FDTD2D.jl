ce236e4aa2295086e00d9bd2f5cf72d361723182
Artem Korzhimanov <korzhimanov.artem@gmail.com>
2017-02-14 12:18:20 +0300

    Implemented generate_fields_x_min!() without tests yet

--- src/FDTD2D.jl
+++ src/FDTD2D.jl
@@ -19,12 +19,13 @@ function calculate_params(init_params::Dict)
   params["half_cfl"]["x"] = 0.5*params["time_step"]/params["space_step"]["x"]
   params["half_cfl"]["y"] = 0.5*params["time_step"]/params["space_step"]["y"]
 
-  params["y1"] = [0.0:params["matrix_size"]["y"]-1.0] * params["space_step"]["y"] + params["y_bounds"][1]
-  params["y2"] = [0.5:params["matrix_size"]["y"]-1.5] * params["space_step"]["y"] + params["y_bounds"][1]
+  params["y1"] = linspace(params["y_bounds"][1], params["y_bounds"][1] + (params["matrix_size"]["y"]-1.0) * params["space_step"]["y"], params["matrix_size"]["y"])
+  params["y2"] = linspace(params["y_bounds"][1]+0.5*params["y_bounds"][1], params["y_bounds"][1] + (params["matrix_size"]["y"]-1.5) * params["space_step"]["y"], params["matrix_size"]["y"]-1)
 
   return params
 end
 
+""""Initialize data"""
 function init_data(params::Dict)
   data = Dict{String, Array{Float64,2}}()
   for name in ("ex","ey","ezx","ezy","ez")
@@ -38,7 +39,18 @@ end
 
 """Generate fields at left boundary (x_min = p["x_bounds"][0]) at time moment 'time'"""
 function generate_fields_x_min!(d::Dict, time::Float64, p::Dict)
-  return true
+  for j = 1:p["matrix_size"]["y"]
+    d["ey"][2,j] -= p["laser_pulse_y_shape"](time, p["x_bounds"][1] + p["space_step"]["x"], p["y1"][j])
+    d["ezx"][2,j] -= p["laser_pulse_z_shape"](time, p["x_bounds"][1] + p["space_step"]["x"], p["y1"][j])
+  end
+
+  for j = 1:p["matrix_size"]["y"]-1
+    d["hzx"][2,j] -= p["laser_pulse_y_shape"](time + 0.5*p["time_step"], p["x_bounds"][1] + 1.5*p["space_step"]["x"], p["y2"][j])
+    d["hy"][2,j] -= p["laser_pulse_z_shape"](time + 0.5*p["time_step"], p["x_bounds"][1] + 1.5*p["space_step"]["x"], p["y2"][j])
+  end
+
+  d["ez"][2,:] = d["ezx"][2,:] + d["ezy"][2,:]
+  d["hz"][2,:] = d["hzx"][2,:] + d["hzy"][2,:]
 end
 
 function make_step!(d::Dict, p::Dict)
